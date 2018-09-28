046dedff1ad99229ee11076f2e133b6e6afec5e9
danb <dan@buzzo.com>
2018-09-28 12:54:34 +0100

    code updates for video grabber

--- session_2/src/ofApp.h
+++ session_2/src/ofApp.h
@@ -2,13 +2,20 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+// This example is  based on the Processing "ASCII Video" example by Ben Fry
+// and it is designed to demonstrate how to re-interpret live video as text
+// to make it look like some good old ASCII art!
+//
+// For more information regarding this example take a look at the README.md.
 
+class ofApp : public ofBaseApp{
+	
 	public:
+		
 		void setup();
 		void update();
 		void draw();
-
+		
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
@@ -19,6 +26,12 @@ class ofApp : public ofBaseApp{
 		void mouseExited(int x, int y);
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+		void gotMessage(ofMessage msg);		
 		
+		ofVideoGrabber vidGrabber;
+		int camWidth;
+		int camHeight;
+    
+        string asciiCharacters;
+        ofTrueTypeFont font;
 };
