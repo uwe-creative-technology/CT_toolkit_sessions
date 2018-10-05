4e02fba554d52bf9c96586c2198ad3213e0abec9
danb <dan@buzzo.com>
2018-10-05 16:28:44 +0100

    initial commit of kinect demo code with comments

--- session_3/src/ofApp.h
+++ session_3/src/ofApp.h
@@ -1,24 +1,72 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
 
-class ofApp : public ofBaseApp{
+// Windows users:
+// You MUST install the libfreenect kinect drivers in order to be able to use
+// ofxKinect. Plug in the kinect and point your Windows Device Manager to the
+// driver folder in:
+//
+//     ofxKinect/libs/libfreenect/platform/windows/inf
+//
+// This should install the Kinect camera, motor, & audio drivers.
+//
+// You CANNOT use this driver and the OpenNI driver with the same device. You
+// will have to manually update the kinect device to use the libfreenect drivers
+// and/or uninstall/reinstall it in Device Manager.
+//
+// No way around the Windows driver dance, sorry.
 
-	public:
-		void setup();
-		void update();
-		void draw();
+// uncomment this to read from two kinects simultaneously
+//#define USE_TWO_KINECTS
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+// Dan Buzzo 2018 - github.com/uwe-creative-technology/
+// for UWE Bristol, Creative Technology MSc, Creative Technology Toolkit module 2018-19
+// with modified portions from oF kinect example;
+
+class ofApp : public ofBaseApp {
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void keyPressed(int key);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    
+    // this is the definition of a custom function that we will use to turn kinect data into a 3d point cloud
+    void drawPointCloud();
+    
+    // here we define an instance of the kinect object to talk to our kinect sensor
+    ofxKinect kinect;
+    
+#ifdef USE_TWO_KINECTS
+    ofxKinect kinect2;
+#endif
+    
+    // define some image objects to store our image data as we work
+    ofxCvColorImage colorImg;
+    ofxCvGrayscaleImage grayImage; // grayscale depth image
+    ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
+    ofxCvGrayscaleImage grayThreshFar; // the far thresholded image    
+    ofxCvContourFinder contourFinder;
+    
+    // define boolean options for how the data will be rendered on screen- it is good practice to begin boolean variabes with a b_ to make your code more readable
+    bool b_ThreshWithOpenCV;
+    bool b_DrawPointCloud;
+    // define variables for filtering the data from the kinect
+    int nearThreshold;
+    int farThreshold;
+    int angle;
+    
+    // define a new virtual camera to be used for viewing our 3d data onscreen
+    ofEasyCam easyCam;
 };
