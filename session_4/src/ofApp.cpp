3cc82469de5c894836b4fe39e9b6172edcaab5a6
danb <dan@buzzo.com>
2018-10-08 16:41:26 +0100

    initial commit

--- session_4/src/ofApp.cpp
+++ session_4/src/ofApp.cpp
@@ -2,7 +2,20 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+//    ofxEspeakSynth::ESParam es;
+//    es.language="en";
+//    es.gender=1;
+//    es.age=85;
+//    spk.setup(es);
+//    
+//    spk.speak("Dario. Pluto. Paperino. Kashimastro. Openframeworks. Opengl. Synth. Voice.");
+    
+//    log.start("/usr/bin/espeak");
+   // log.start("say");
+//
+  //  log.LogAudio("Hello World");
+    
+    system("say 'this is a quick test' &");
 }
 
 //--------------------------------------------------------------
@@ -17,7 +30,8 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+//    if(key == ' ')
+//        spk.speak("keyboard pressed!");
 }
 
 //--------------------------------------------------------------
@@ -42,6 +56,9 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+    string message;
+    message ="mouse is at";
+    system("say message &");
 
 }
 
--- session_4/src/ofApp.h
+++ session_4/src/ofApp.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxEspeakSynth.h"
 
 class ofApp : public ofBaseApp{
 
+   
+    
 	public:
 		void setup();
 		void update();
@@ -20,5 +23,41 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+    
+    // create instance of an Espeak object
+   // ofxEspeakSynth spk;
+    
+    // requires https://downloads.sourceforge.net/project/espeak/espeak/espeak-1.45/espeak-1.45.04-OSX.zip?r=&ts=1538935584&use_mirror=kent
+};
+
+class ofLogAudio : public ofThread{
+public:
+    bool startSpeak = false;
+    string msg;
+    string speak;
+    
+    void start(string _c="/usr/bin/espeak -v mb-it3")
+    {
+        speak = _c;
+        startThread(true);
+    }
+    
+    void LogAudio(string _msg)
+    {
+        msg = _msg;
+        startSpeak = true;
+    }
+    
+    void threadedFunction()
+    {
+        while(isThreadRunning())
+        {
+            if(startSpeak)
+            {
+                ofSystem(speak+" '"+msg+"' > /dev/null 2>&1");
+                startSpeak = false;
+            }
+            
+        }
+    }
 };
