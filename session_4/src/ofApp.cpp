b7a6c9149bfaf0462217293b848a67fb0ca97c77
danb <dan@buzzo.com>
2018-10-16 09:52:39 +0200

    updated osx system speech example

--- session_4/src/ofApp.cpp
+++ session_4/src/ofApp.cpp
@@ -11,7 +11,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
     synth.load("ghostpad.aif");  // load sound files from data folder into sound objects
     beats.load("percussion.aif");
     synth.setVolume(0.75f); // set the parameters for sound objects
@@ -51,7 +51,13 @@ void ofApp::keyPressed(int key){
             synth.play();
             break;
             
-        default:
+        case '4':
+            // example of passing content and a voice selection instruction to the system 'say' command
+            string speaker = "Samantha";
+            string singer = "Bad News"; // check which voices are loaded in osx system preferences
+            string content = "oh dear, that's not good " + ofToString( ofGetDay()) + " '" ;
+            string cmd = "say " + content + " -v " + speaker;
+            system(cmd.c_str());
             break;
     }
 }
@@ -63,7 +69,12 @@ void ofApp::mousePressed(int x, int y, int button){
         synth.play();
         system("say 'synth' &");
         synth.setSpeed( 0.1f + ((float)(ofGetHeight() - y) / (float)ofGetHeight())*10);
-        synth.setPan(ofMap(x, 0, widthStep, -1, 1, true));
+        // synth.setPan(ofMap(x, 0, widthStep, -1, 1, true));
+        synth.setVolume(ofMap(y, 0, ofGetHeight(), 0, 1));
+        // here we use a MAP funtion to map a range of input values to a range of desired output values
+        // in our case we take in the mouseY position (range from 0 to the screen height) and MAP this to values from 0 to 1 that we can use to send to the synth.setVolume() function
+        // the syntax for the oF map function is as follows:
+        // ofMap(<#float value#>, <#float inputMin#>, <#float inputMax#>, <#float outputMin#>, <#float outputMax#>)
     } else if (x >= widthStep && x < widthStep*2){
         beats.play();
         system("say 'beats' &");
