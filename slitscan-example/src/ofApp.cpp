dee423dca2bdaac633304d2c1d044417bcc8cea9
podcast <podcast@db.lan>
2019-09-30 13:11:09 +0200

    updated simplified example

--- slitscan-example/src/ofApp.cpp
+++ slitscan-example/src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup(){
     camWidth =  640;  // try to grab at this size from the camera.
     camHeight = 480;
-    y = 0;
+    xSteps = 0;
     
     // ask the video grabber for a list of attached camera devices.
     // put it into a vector of devices
@@ -22,101 +22,47 @@ void ofApp::setup(){
     // set the ID of the camera we will use
     vidGrabber.setDeviceID(0);
     // set how fast we will grab frames from the camera
-    vidGrabber.setDesiredFrameRate(60);
+    vidGrabber.setDesiredFrameRate(30);
     // set the width and height of the camera
     vidGrabber.initGrabber(camWidth, camHeight);
     // set up our pixel object to be the same size as our camera object
     videoInverted.allocate(camWidth,camHeight, OF_PIXELS_RGB);
     videoTexture.allocate(videoInverted);
     ofSetVerticalSync(true);
+    
+    ofBackground(100, 100, 100); // set the background colour to dark grey
 }
-
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    ofBackground(100, 100, 100);
-    vidGrabber.update();
     
+    vidGrabber.update();
     ofPixels & pixels = vidGrabber.getPixels();
     
-    for (int x=0; x<camWidth; x++ ) { // loop through all the pixels on a line
-        ofColor color = pixels.getColor( x,  y); // get the pixels on line y
-        videoInverted.setColor(x, y, color);
-        
+    for (int y=0; y<camWidth; y++ ) { // loop through all the pixels on a line
+        ofColor color = pixels.getColor( xSteps, y); // get the pixels on line ySteps
+        videoInverted.setColor(xSteps, y, color);
     }
+    
     videoTexture.loadData(videoInverted);
     
-    if (y>=camHeight) {
-        y=0; // if we are on the bottom line of the image then start at the top again
-    } else {
-        y+=1; // otherwise step on to the next line.
-        // y+=2; // uncomment this instead to step on two lines at a time.
-
+    if ( xSteps >= camHeight ) {
+        xSteps = 0; // if we are on the bottom line of the image then start at the top again
     }
+    xSteps += 1; // step on to the next line. increase this number to make things faster
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofSetHexColor(0xffffff);
-    vidGrabber.draw(0, 0);
-    videoTexture.draw( camWidth, 0, camWidth, camHeight);
+    vidGrabber.draw(0, 0); // draw our plain image
+    videoTexture.draw( camWidth, 0, camWidth, camHeight); // draw the video texture we have constructed
 }
-
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    // in fullscreen mode, on a pc at least, the
-    // first time video settings the come up
-    // they come up *under* the fullscreen window
-    // use alt-tab to navigate to the settings
-    // window. we are working on a fix for this...
     
-    // Video settings no longer works in 10.7
-    // You'll need to compile with the 10.6 SDK for this
-    // For Xcode 4.4 and greater, see this forum post on instructions on installing the SDK
-    // http://forum.openframeworks.cc/index.php?topic=10343
-    if(key == 's' || key == 'S'){
-        vidGrabber.videoSettings();
-        
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
+    if(key == 'f' || key == 'F'){
+        ofToggleFullscreen();
+    }    
 }
